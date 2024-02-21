b300f466c8907e0e033ec457bad851833704bbe6
Mukunda <borah.mukunda96@gmail.com>
2024-02-21 22:37:30 +0530

    added signIn page, signIn with Fb, number and google

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,67 @@
+import { Container, Grid, Row, Panel, Col, Button, Icon, Alert } from 'rsuite';
+import firebase from 'firebase/app';
+import { auth, database } from '../misc/firebase';
+
 const SignIn = () => {
-  return <div>SignIn</div>;
+  const signInWithProvider = async provider => {
+    try {
+      const { additionalUserInfo, user } = await auth.signInWithPopup(provider);
+
+      if (additionalUserInfo.isNewUser) {
+        await database.ref(`/profiles/${user.uid}`).set({
+          name: user.displayName,
+          createdAt: firebase.database.ServerValue.TIMESTAMP,
+        });
+      }
+
+      Alert.success('Signed in', 4000);
+    } catch (err) {
+      Alert.error(err.message, 4000);
+    }
+  };
+
+  const onFacebookSignIn = () => {
+    signInWithProvider(new firebase.auth.FacebookAuthProvider());
+  };
+
+  const onGoogleSignIn = () => {
+    signInWithProvider(new firebase.auth.GoogleAuthProvider());
+  };
+
+  const onPhoneSignIn = () => {
+    signInWithProvider(new firebase.auth.PhoneAuthProvider());
+  };
+
+  return (
+    <Container>
+      <Grid className="mt-page">
+        <Row>
+          <Col xs={24} md={12} mdOffset={6}>
+            <Panel>
+              <div className="text-center">
+                <h2>Welcome to chat</h2>
+                <p>Progressive chat platform for Mukunda </p>
+              </div>
+
+              <div className="mt-3">
+                <Button block color="blue" onClick={onFacebookSignIn}>
+                  <Icon icon="facebook" /> Continue with facebook
+                </Button>
+
+                <Button block color="green" onClick={onGoogleSignIn}>
+                  <Icon icon="google" /> Continue with Google
+                </Button>
+
+                <Button block color="orange" onClick={onPhoneSignIn}>
+                  <Icon icon="phone" /> Continue with PhoneNumber
+                </Button>
+              </div>
+            </Panel>
+          </Col>
+        </Row>
+      </Grid>
+    </Container>
+  );
 };
 
 export default SignIn;
