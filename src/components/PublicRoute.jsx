487388fa8a388feb55531606242940f830963408
Mukunda <borah.mukunda96@gmail.com>
2024-02-22 20:33:24 +0530

    managed user with profile context put real time sub

--- src/components/PublicRoute.jsx
+++ src/components/PublicRoute.jsx
@@ -1,9 +1,19 @@
 import { Route, Redirect } from 'react-router';
+import { useProfile } from '../context/profile.context';
+import { Container, Loader } from 'rsuite';
 
 const PublicRoute = ({ children, ...routeProps }) => {
-  const profile = false;
+  const { profile, isLoading } = useProfile();
 
-  if (profile) {
+  if (isLoading && !profile) {
+    return (
+      <Container>
+        <Loader center vertical size="md" content="Loading" speed="slow" />
+      </Container>
+    );
+  }
+
+  if (profile && !isLoading) {
     return <Redirect to="/" />;
   }
 
