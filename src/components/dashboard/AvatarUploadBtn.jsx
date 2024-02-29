0dee7056e46c674f6612b1f8f0cde8b0c63ac639
Mukunda <borah.mukunda96@gmail.com>
2024-02-29 21:00:01 +0530

    displayed avatar user avatar

--- src/components/dashboard/AvatarUploadBtn.jsx
+++ src/components/dashboard/AvatarUploadBtn.jsx
@@ -4,6 +4,7 @@ import { useState, useRef } from 'react';
 import AvatarEditor from 'react-avatar-editor';
 import { useProfile } from '../../context/profile.context';
 import { database, storage } from '../../misc/firebase';
+import ProfileAvatar from '../ProfileAvatar';
 
 const fileInputTypes = '.png, .jpeg, .jpg ';
 
@@ -81,6 +82,12 @@ const AvatarUploadBtn = () => {
 
   return (
     <div className="mt-3 text-center">
+      <ProfileAvatar
+        src={profile.avatar}
+        name={profile.name}
+        className="width-200 height-200 img-fullsize font-huge"
+      />
+
       <div>
         <label
           htmlFor="avatar-upload"
